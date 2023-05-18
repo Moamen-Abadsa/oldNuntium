53b19c99955b1243c4811035a8f9ded2620ebc84
Moamen-Abadsa <101631697+Moamen-Abadsa@users.noreply.github.com>
2023-05-18 09:33:30 +0300

    Add existing project files to Git

---
+++ lib/config/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const String baseURL =
+      "https://newsapi.org/v2/everything?q=tesla&from=2023-04-18&sortBy=publishedAt&apiKey=$API_KEY";
+  static const String API_KEY = "0ffcced1a5c44be2b2f4984ec3b404cd";
+}
