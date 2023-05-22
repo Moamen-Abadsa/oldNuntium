23535e824585ed29b591b0f236861f3b3167ed66
Moamen-Abadsa <101631697+Moamen-Abadsa@users.noreply.github.com>
2023-05-22 19:27:10 +0300

    add some assets and files

---
+++ lib/core/resorces/manager_assets.dart
@@ -0,0 +1,14 @@
+const String baseImagePath = 'assets/images';
+const String baseVectorPath = 'assets/images/vectors';
+
+class ManagerAssets {
+  static const String logo = '$baseVectorPath/Vector.svg';
+  static const String outboarding = '$baseImagePath/outboarding.png';
+  static const String illustration = '$baseImagePath/3d-illustration.png';
+}
+
+class ManagerJson {
+  // static const String loading = '$jsonPath/loading.json';
+  // static const String error = '$jsonPath/error.json';
+  // static const String success = '$jsonPath/success.json';
+}
---
+++ lib/core/resorces/manager_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ManagerColors {
+  static const Color White = Colors.white;
+  static const Color PrimaryColor = Color(0x475AD7);
+  static const Color PrimaryDarkColor = Color(0x2536A7);
+  static const Color LightPrimaryColor = Color(0x8A96E5);
+  static const Color LighterPrimaryColor = Color(0xEEF0FB);
+  static const Color PrimaryBlackColor = Color(0x333647);
+  static const Color LighterBlackColor = Color(0x555A77);
+  static const Color DarkerBlackColor = Color(0x22242F);
+  static const Color PrimaryGreyColor = Color(0x7C82A1);
+  static const Color Black = Colors.black;
+}
+
+//ليش احنا بنعرف ألوان سهلة زي الأبيض مثلا!
