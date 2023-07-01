bfcd2725d9e9335ff5c6bbb4daebf088211e71de
Moamen-Abadsa <101631697+Moamen-Abadsa@users.noreply.github.com>
2023-07-01 23:48:43 +0300

    edit login_controller.dart , login_view.dart , add auth_view  widget , add register_controller.dart , register_view.dart , footer_message.dart , etc

---
+++ lib/features/auth/presentation/view/register_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nuntium/core/resorces/manager_strings.dart';
+import 'package:nuntium/features/auth/presentation/controller/register_controller.dart';
+import 'package:nuntium/features/auth/presentation/view/widgets/auth_view.dart';
+import 'package:nuntium/features/auth/presentation/view/widgets/footer_message.dart';
+import 'package:nuntium/routes/routes.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<RegisterController>(
+      builder: (controller) {
+        return authView(
+          title: ManagerStrings.signUpTitle,
+          paragraph: ManagerStrings.signUpParagraph,
+          buttonText: ManagerStrings.signUp,
+          password: true,
+          confirmPassword: true,
+          name: true,
+          child: footerMessage(
+              destination: Routes.loginView,
+              firstMessage: ManagerStrings.signUpFooterMessage,
+              secondMessage: ManagerStrings.signIn),
+          controllers: {
+            'passwordController': controller.passwordController,
+            'emailController': controller.emailController,
+            'nameController': controller.nameController,
+            'confirmPasswordController': controller.confirmPasswordController,
+          },
+        );
+      },
+    );
+  }
+}
