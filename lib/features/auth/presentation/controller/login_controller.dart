04a4e391c002a315045d4c1e09b7833ea8972801
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-03 19:00:57 +0300

    solving some merge conflicts

--- lib/features/auth/presentation/controller/login_controller.dart
+++ lib/features/auth/presentation/controller/login_controller.dart
@@ -4,16 +4,16 @@ import 'package:nuntium/config/dependency_injection.dart';
 import 'package:nuntium/features/auth/domain/use_case/login_use_case.dart';
 
 class LoginController extends GetxController {
-  late TextEditingController email = TextEditingController();
-  late TextEditingController password = TextEditingController();
+  late TextEditingController emailController = TextEditingController();
+  late TextEditingController passwordController = TextEditingController();
   late final LoginUseCase _loginUseCase = instance<LoginUseCase>();
   var formKey = GlobalKey<FormState>();
 
   Future<void> login() async {
     (await _loginUseCase.execute(
       LoginUseCaseInput(
-        email: email.text,
-        password: password.text,
+        email: emailController.text,
+        password: passwordController.text,
       ),
     ))
         .fold(
--- lib/features/auth/presentation/controller/register_controller.dart
+++ lib/features/auth/presentation/controller/register_controller.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RegisterController extends GetxController {
-  
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
 }
