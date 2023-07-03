4b5c3002a71361b68712449c288c84ff8d2395d0
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-03 22:36:07 +0300

    Refactoring: changing parameter name

--- lib/features/forget_password/data/data_source/remote_forget_password_data_source.dart
+++ lib/features/forget_password/data/data_source/remote_forget_password_data_source.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nuntium/features/forget_password/data/request/forget_password_request.dart';
 
 abstract class RemoteForgetPasswordDataSource {
-  Future<void> forgetPassword(ForgetPasswordRequest loginRequest);
+  Future<void> forgetPassword(ForgetPasswordRequest forgetPasswordRequest);
 }
 
 class RemoteForgetPasswordDataSourceImplement implements RemoteForgetPasswordDataSource {
