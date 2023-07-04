db65ff23a65f8f7f02a261014eb0d4e5e499e204
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-04 15:58:20 +0300

    some changes in SelectFavoriteTopicController

--- lib/features/favorite_topic/presentation/controller/select_favorite_topic_controller.dart
+++ lib/features/favorite_topic/presentation/controller/select_favorite_topic_controller.dart
@@ -1,19 +1,42 @@
 import 'package:get/get.dart';
 import 'package:nuntium/config/dependency_injection.dart';
+import 'package:nuntium/features/favorite_topic/domain/mapper/topics_entity_mapper.dart';
 import 'package:nuntium/features/favorite_topic/domain/use_case/select_favorite_topic_use_case.dart';
+import 'package:nuntium/features/favorite_topic/domain/use_case/topics_use_case.dart';
+import 'package:nuntium/features/favorite_topic/presentation/models/topic.dart';
 
 class SelectFavoriteTopicController extends GetxController {
   late final _selectFavoriteTopicUseCase = instance<SelectFavoriteTopicUseCase>();
+  late final _topicsUseCase = instance<TopicsUseCase>();
+
+  List<Topic> topics = [];
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchTopics();
+  }
 
   Future<void> selectFavoriteTopic() async {
     (await _selectFavoriteTopicUseCase.execute(
-      SelectFavoriteTopicUseCaseInput(topic: ''),
+      //Todo enter the ids of the selected topics here
+      SelectFavoriteTopicUseCaseInput(topics: []),
     ))
         .fold(
       // Todo: حالة الفشل
       (l) => null,
       //Todo: حالة النجاح
       (r) => null,
+    );
+  }
+
+  Future<void> fetchTopics() async {
+    (await _topicsUseCase.execute()).fold(
+      // Todo: حالة الفشل
+      (l) => null,
+
+      //Todo: حالة النجاح
+      (r) => topics = r.toPresentation(),
     );
   }
 }
