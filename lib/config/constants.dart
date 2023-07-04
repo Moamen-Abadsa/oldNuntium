e76db7b17cde9a4c269bb3c2617d28eb0387c1bb
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-04 19:41:44 +0300

    fix: issue caused by merge

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -21,9 +21,7 @@ class FirestoreConstants {
   static const String topics = "categories";
 }
 
-class FirestoreConstants {
-  static const String topics = "categories";
-}
+
 
 class ApiConstants {
   static const String API_KEY = "0ffcced1a5c44be2b2f4984ec3b404cd";
