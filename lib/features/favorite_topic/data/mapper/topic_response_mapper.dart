c8cb410c0519e81fac012bab2097c931d19e0b87
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-04 15:23:51 +0300

    Add: Topic Resopnse Mapper

---
+++ lib/features/favorite_topic/data/mapper/topic_response_mapper.dart
@@ -0,0 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nuntium/features/favorite_topic/domain/entity/topic_entity.dart';
+
+extension TopicResopnseMapper on QueryDocumentSnapshot<Map<String, dynamic>> {
+  TopicEntity topicDocToDomain() {
+    return TopicEntity(name: id);
+  }
+}
