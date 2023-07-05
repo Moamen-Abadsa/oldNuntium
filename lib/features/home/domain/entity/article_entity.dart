2ce67be72356e6c1b9091e80383a7129de113f29
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-05 17:21:40 +0300

    Add: ArticleEntity

---
+++ lib/features/home/domain/entity/article_entity.dart
@@ -0,0 +1,19 @@
+class ArticleEntity {
+  String author;
+  String publishedAt;
+  String content;
+  String imageUrl;
+  String url;
+  String description;
+  String title;
+
+  ArticleEntity({
+    required this.author,
+    required this.publishedAt,
+    required this.content,
+    required this.imageUrl,
+    required this.url,
+    required this.description,
+    required this.title,
+  });
+}
