bd26f78b2e412db846ee8ee3482a4b462527c342
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-05 17:22:20 +0300

    Add:  ArticleEntityMapper toPresentation

---
+++ lib/features/home/domain/mapper/article_mapper.dart
@@ -0,0 +1,16 @@
+import '../../domain/entity/article_entity.dart';
+import '../../presentation/model/article.dart';
+
+extension ArticleEntityMapper on ArticleEntity {
+  Article toPresentation() {
+    return Article(
+      author: author,
+      content: content,
+      description: description,
+      imageUrl: imageUrl,
+      publishedAt: publishedAt,
+      title: title,
+      url: url,
+    );
+  }
+}
