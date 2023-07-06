530c82af9e05dd1a5093a67d7ba9cae2e4e79e99
Muhanned-Anwar <mohannedanwarflutter@gmail.com>
2023-07-07 02:10:07 +0300

    fix bug

--- lib/features/auth/data/repository/login_repository.dart
+++ lib/features/auth/data/repository/login_repository.dart
@@ -1,12 +1,14 @@
 import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:nuntium/core/error_handler/error_handler.dart';
 import 'package:nuntium/core/error_handler/response_code.dart';
 import 'package:nuntium/core/internet_checker/internet_checker.dart';
 import 'package:nuntium/core/resorces/manager_strings.dart';
 import 'package:nuntium/features/auth/data/data_source/remote_login_data_source.dart';
 import 'package:nuntium/features/auth/data/request/login_request.dart';
+import 'package:nuntium/features/auth/data/response/login_response.dart';
 
 abstract class LoginRepository {
   Future<Either<Failure, void>> login(LoginRequest loginRequest);
@@ -22,18 +24,15 @@ class LoginRepositoryImplement implements LoginRepository {
   );
 
   @override
-  Future<Either<Failure, void>> login(LoginRequest loginRequest) async {
+  Future<Either<Failure, LoginResponse>> login(LoginRequest loginRequest) async {
     if (await _networkInfo.isConnected) {
       try {
-        await _remoteLoginDataSource.login(loginRequest);
-      } catch (e) {
-        debugger();
+        return Right(await _remoteLoginDataSource.login(loginRequest));
+      } on FirebaseAuthException catch (e) {
         return Left(
           ErrorHandler.handle(e).failure,
         );
       }
-
-      return const Right(null);
     } else {
       return Left(
         Failure(
