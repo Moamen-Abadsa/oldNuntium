79763d7a597ec8f025844ac60545050165ed9171
Moamen-Abadsa <101631697+Moamen-Abadsa@users.noreply.github.com>
2023-07-06 18:42:03 +0300

    add language widgets and strings to be translated

--- lib/core/resorces/manager_assets.dart
+++ lib/core/resorces/manager_assets.dart
@@ -1,5 +1,6 @@
 const String baseImagePath = 'assets/images';
 const String baseVectorPath = 'assets/images/vectors';
+const String translationPath = 'assets/translation';
 
 class ManagerAssets {
   static const String logo = '$baseVectorPath/Vector.svg';
@@ -9,6 +10,7 @@ class ManagerAssets {
   static const String google = '$baseImagePath/google-logo.png';
   static const String white_house = '$baseImagePath/white_house.png';
   static const String news2 = '$baseImagePath/news2.png';
+  static const String news_placeholder = '$baseImagePath/news_placeholder.jpg';
 }
 
 class ManagerJson {
