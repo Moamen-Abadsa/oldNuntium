8da341725886d64580944f1811c53639036e7426
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-07-07 18:33:07 +0300

    Refactor: adding comments

--- lib/features/splash/controller/splash_controller.dart
+++ lib/features/splash/controller/splash_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 import 'package:nuntium/config/constants.dart';
 import 'package:nuntium/config/dependency_injection.dart';
@@ -5,28 +7,36 @@ import 'package:nuntium/core/storage/local/app_settings_shared_preferences.dart'
 import 'package:nuntium/routes/routes.dart';
 
 class SplashController extends GetxController {
-  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
-      instance<AppSettingsSharedPreferences>();
+  final AppSettingsSharedPreferences _appSettingsSharedPreferences = instance<AppSettingsSharedPreferences>();
 
   @override
   void onInit() {
     super.onInit();
     Future.delayed(
-        const Duration(
-          seconds: Constants.splashDuration,
-        ), () {
-      if (_appSettingsSharedPreferences.getOutBoardingViewed()) {
-        if (_appSettingsSharedPreferences.loggedIn()) {
-          Get.offAllNamed(Routes.homeView);
+      const Duration(
+        seconds: Constants.splashDuration,
+      ),
+      () {
+        ///if the OutBoarding is viewd
+        if (_appSettingsSharedPreferences.getOutBoardingViewed()) {
+          debugger();
+          if (_appSettingsSharedPreferences.loggedIn()) {
+            debugger();
+
+            ///if User is loggedIn
+            Get.offAllNamed(Routes.mainView);
+
+            ///if User is NOT loggedIn
+          } else {
+            Get.offAllNamed(Routes.loginView);
+          }
+
+          ///if the OutBoarding is NOT viewd
         } else {
-          Get.offAllNamed(Routes.loginView);
+          Get.offAllNamed(Routes.outBoardingView);
         }
-      } else {
         Get.offAllNamed(Routes.outBoardingView);
-      }
-      Get.offAllNamed(
-        Routes.outBoardingView,
-      );
-    });
+      },
+    );
   }
 }
